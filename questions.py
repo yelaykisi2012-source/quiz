de47fb94c610729a8eba6132330224d69fb9efcf
yelaykisi2012-source <yelaykisi2012@gmail.com>
2026-02-28 13:52:06 +0500

    Create questions.py

---
+++ questions.py
@@ -0,0 +1,21 @@
+# questions.py
+
+# Python savollari ro'yxati
+quiz_data = [
+    {
+        "code": "print(2 + 2)",
+        "options": ["2", "4", "22"],
+        "correct_index": 1,
+        "explanation": "Matematikada 2+2=4 bo'ladi."
+    }
+]
+
+# JavaScript savollari ro'yxati - SHU YERDA 'def' BO'LMASIN!
+js_quiz_data = [
+    {
+        "code": "console.log(1 + '1')",
+        "options": ["2", "11", "Error"],
+        "correct_index": 1,
+        "explanation": "JSda son va satr qo'shilsa, ular birlashib ketadi."
+    }
+]
